46079e7c3ddacfebd5955315c8f418c4498853cb
Richarlyson Ramos Grolla <richarlysonrg@gmail.com>
2025-10-14 20:53:24 -0300

    novos perfis

--- util/perfis.py
+++ util/perfis.py
@@ -17,7 +17,8 @@ class Perfil(str, Enum):
 
     # PERFIS DO SEU SISTEMA #####################################
     ADMIN = "Administrador"
-    CLIENTE = "Cliente"
+    PROFESSOR = "Professor"
+    ALUNO = "Aluno"
     # FIM DOS PERFIS ############################################
 
     def __str__(self) -> str:
